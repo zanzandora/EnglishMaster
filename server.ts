c9311e5439e03015fe227c68018ef74d2926302b
YMGPwcca <hiimnghia.real@gmail.com>
2025-01-09 23:58:00 +0700

    update to ssr vite

---
+++ server.ts
@@ -0,0 +1,46 @@
+// Trimmed down version of https://github.com/bluwy/create-vite-extra/blob/master/template-ssr-react-ts/server.js
+
+import fs from 'node:fs/promises'
+import express from 'express'
+import { ViteDevServer } from 'vite'
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const app = express()
+app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+let vite: ViteDevServer
+if (!isProduction) {
+  const { createServer } = await import('vite')
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+    base: '/',
+  })
+  app.use(vite.middlewares)
+}
+else {
+  app.use((await import('compression')).default())
+  app.use('/', (await import('sirv')).default('./dist/client', { extensions: [] }))
+}
+
+app.use('*all', async (req, res) => {
+  try {
+    const url = req.originalUrl.replace('/', '')
+
+    let html = !isProduction
+      ? await vite.transformIndexHtml(url, await fs.readFile('./index.html', 'utf-8'))
+      : await fs.readFile('./dist/client/index.html', 'utf-8')
+
+    res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
+  }
+  catch (e) {
+    vite?.ssrFixStacktrace(e)
+    console.log(e.stack)
+    res.status(500).end(e.stack)
+  }
+})
+
+app.listen(5173, () => console.log(`Server started at http://localhost:5173`))
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { hydrateRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App'
+
+import './index.css'
+
+hydrateRoot(
+  document.getElementById('root') as HTMLElement,
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='*' element={<>404 Not Found</>} />
+    </Routes>
+  </BrowserRouter>
 )
