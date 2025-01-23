8e1565c5c8d0a7d8b11881755819a2dd250c6874
theLazyBoy_mmTu <114607201+zanzandora@users.noreply.github.com>
2025-01-24 00:01:07 +0700

    config colors in tailwinds.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./client/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './client/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#ff4742',
+          light: '#fff',
+          dark: '#1E40AF',
+        },
+      },
+    },
   },
   plugins: [],
-}
-
+};
